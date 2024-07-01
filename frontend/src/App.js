0be798261dc6a7315971d0322a26286a0c1dc4fa
Rauf Payyzou <raufed.net@bk.ru>
2024-07-01 23:06:56 +0300

    Frontend update. A delete button has been added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Button, ButtonGroup, Table } from 'reactstrap';
 import './App.css';
 import React, { Component, useEffect, useState } from 'react';
 
@@ -6,34 +7,57 @@ class App extends Component {
     super(props);
   }
 
-  state = {    dishes: []};
-    async componentDidMount() {
-      try {
-        const response = await fetch("http://localhost:8080/api/v1/menu");
-        console.log(response);
-        console.log("Данные должны быть тут: ", this.state);
-        const jsonData = await response.json();
-        this.setState({ dishes: jsonData });
-      } catch (error) {
-        console.error("Ошибка при получении данных: ", error);
-      }
+  state = { dishes: [] };
+  async componentDidMount() {
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/menu");
+      const jsonData = await response.json();
+      this.setState({ dishes: jsonData });
+    } catch (error) {
+      console.error("Ошибка при получении данных: ", error);
     }
+  }
+
+  async remove(id) {
+    await fetch(`http://localhost:8080/api/v1/menu/${id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(() => {
+      let updatedDish = [...this.state.dishes].filter(i => i.id !== id);
+      this.setState({ dishes: updatedDish })
+    });
+  }
+
   render() {
-    const {dishes} = this.state;
-    console.log(this.state);
-    console.log(dishes);
+    const { dishes } = this.state;
+    const dishList = dishes.map(dish =>
+      <div key={dish.id} className="popup-menu">
+        <div className="info">
+          <div className="dish-name">{dish.name}</div>
+          <div className="dish-price">{dish.price} рублей</div>
+          <div className="actions">
+            <Button size="sm" color="danger" onClick={() => this.remove(dish.id)}>Удалить</Button>
+          </div>
+        </div>
+      </div>
+    );
+
     return (
       <div className="App">
+        <h1>ТЕННИС БАР</h1>
         <header className="App-header">
-          <div className="App-intro">
-            <h2>menu</h2>
-            {dishes.map(dish =>
-              <div key={dish.id}>
-                {dish.name} ({dish.price})
-              </div>
-            )}
-          </div>
+          <h2>Меню</h2>
         </header>
+        <div className="App-intro">
+          {
+            dishList
+          }
+        </div>
       </div>
     );
   }
