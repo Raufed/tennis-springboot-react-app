8493333b7d110499f41bdfecfb72e6f274a40735
Rauf Payyzou <raufed.net@bk.ru>
2024-07-01 15:08:08 +0300

    finished CRUD

--- src/main/java/by/payyzau/tennis/controller/MenuController.java
+++ src/main/java/by/payyzau/tennis/controller/MenuController.java
@@ -16,13 +16,32 @@ import java.util.List;
 public class MenuController {
     @Autowired
     private DishService dishService;
-    @GetMapping()
+    @GetMapping
     public List<Dish> getAll() {
         return dishService.allDish();
     }
-    @PostMapping("createDish")
+    @GetMapping("/{id}")
+    public ResponseEntity<Dish> getDishByID(@PathVariable("id") Long id) throws URISyntaxException{
+        Dish dishById = dishService.getDishById(id);
+        URI location = new URI("api/v1/menu/" + dishById.getId());
+        return ResponseEntity.ok().location(location).body(dishById);
+    }
+    @PostMapping
     public ResponseEntity<Dish> addDish(@RequestBody Dish dish) throws URISyntaxException {
         Dish savedDish = dishService.saveDish(dish);
-        return ResponseEntity.created(new URI("api/v1/menu/"+savedDish.getId())).body(savedDish);
+        return ResponseEntity.created(new URI("api/v1/menu/" + savedDish.getId())).body(savedDish);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Dish> updateDish(@PathVariable("id") Long id, @RequestBody Dish dish) throws URISyntaxException {
+        Dish updatedDish = dishService.updateDish(id, dish);
+        URI location = new URI("api/v1/menu/" + id);
+        return ResponseEntity.ok().location(location).body(updatedDish);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteDish(@PathVariable("id") Long id) throws URISyntaxException {
+         dishService.deleteDish(id);
+         URI location = new URI("api/v1/menu/" + id);
+         return ResponseEntity.ok().location(location).body("Dish with id = " + id + " was deleted!");
     }
 }
--- src/main/java/by/payyzau/tennis/service/DishService.java
+++ src/main/java/by/payyzau/tennis/service/DishService.java
@@ -1,8 +1,11 @@
 package by.payyzau.tennis.service;
 
 import by.payyzau.tennis.entity.Dish;
+import by.payyzau.tennis.exception.NotFoundException;
 import by.payyzau.tennis.repository.DishRepositoty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +15,26 @@ public class DishService {
     @Autowired
     private DishRepositoty dishRepositoty;
 
+    public Dish getDishById(Long id) {
+        Dish dish = dishRepositoty.findById(id).orElseThrow(() -> new NotFoundException("Dish not found!"));
+        return dish;
+    }
     public List<Dish> allDish () {
         return dishRepositoty.findAll();
     }
     public Dish saveDish(Dish dish) {
         return dishRepositoty.save(dish);
+    }
+    public void deleteDish(Long id) {
+        dishRepositoty.deleteById(id);
+    }
+
+    public Dish updateDish(Long id, Dish dish) {
+        Dish currentDish = dishRepositoty.findById(id).orElseThrow(() -> new NotFoundException("Dish not found!"));
+        currentDish.setName(dish.getName());
+        currentDish.setPrice(dish.getPrice());
+        currentDish = dishRepositoty.save(currentDish);
+
+        return  currentDish;
     }
 }
