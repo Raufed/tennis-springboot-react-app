69ecdbf4d8b6ee71a6dbee76d105d784f7156521
Rauf Payyzou <raufed.net@bk.ru>
2024-07-30 12:46:42 +0300

    test2 Travis CI

--- src/main/java/by/payyzau/tennis/TennisApplication.java
+++ src/main/java/by/payyzau/tennis/TennisApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TennisApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TennisApplication.class, args);
 	}
